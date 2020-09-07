95f7383d66f09d32798cffa06daa1958577fb192
he_yulin <he_yulin@163.com>
2020-09-08 00:23:24 +0800

    dd

--- maintain/filebeat/output/mysql.py
+++ maintain/filebeat/output/mysql.py
@@ -10,25 +10,43 @@ import json
 
 class MysqlHander(logging.Handler):
     def __init__(self,host,port, user,pswd,db_name):
-        self.mysql = pymysql.connect(
-            host=host,
-               port=int(port),
-               user=user,
-               passwd=pswd,
-               db=db_name,
-               charset='utf8' )
+        self.host =host
+        self.port = port
+        self.user = user
+        self.pswd = pswd
+        self.db_name = db_name
+        
         self.hostName = socket.gethostname()
         super().__init__()
+        self.connect()
         #print('elk-log1')
     
-    @property
-    def connection(self):
-        general_log.debug('read mysql connection property')
-        if not self.mysql.open:
-            general_log.debug('into mysql reconnect')
-            self.mysql.ping(reconnect=True)
-        return self.mysql
-
+    def connect(self):
+        self.conn = pymysql.connect(
+            host= self.host,
+               port=int(self.port),
+               user=self.user,
+               passwd=self.pswd,
+               db=self.db_name,
+               charset='utf8' )        
+    #@property
+    #def connection(self):
+        #general_log.debug('read mysql connection property')
+        #if not self.mysql.open:
+            #general_log.debug('into mysql reconnect')
+            #self.mysql.ping(reconnect=True)
+        #return self.mysql
+    
+    def executemany(self, sql,ls):
+        try:
+            cursor = self.conn.cursor()
+            cursor.executemany(sql,ls)
+        except pymysql.OperationalError:
+            self.connect()
+            cursor = self.conn.cursor()
+            cursor.executemany(sql,ls)
+        return cursor    
+    
     def send(self,lines):
         actions=[ ]
         ls =[]
@@ -38,9 +56,9 @@ class MysqlHander(logging.Handler):
                 ) 
             
         if ls: 
-            with self.connection.cursor() as cursor:
-                cursor.executemany("insert into act_log_generallog(createtime, level ,message,path,process,host) values (%s, %s,%s,%s,%s,%s)", ls )
-                self.mysql.commit()        
+            #with self.connection.cursor() as cursor:
+            self.executemany("insert into act_log_generallog(createtime, level ,message,path,process,host) values (%s, %s,%s,%s,%s,%s)", ls )
+            self.conn.commit()        
 
 class TableMysqlHander(MysqlHander):
     def send(self,lines):
@@ -56,8 +74,8 @@ class TableMysqlHander(MysqlHander):
                 ) 
             
         if ls: 
-            with self.connection.cursor() as cursor:
-                cursor.executemany("insert into act_log_backendoperation(createtime, model, content , createuser) values (%s,%s,%s,%s)", ls )
-                self.mysql.commit()       
+            #with self.connection.cursor() as cursor:
+            self.executemany("insert into act_log_backendoperation(createtime, model, content , createuser) values (%s,%s,%s,%s)", ls )
+            self.conn.commit()       
 
 
